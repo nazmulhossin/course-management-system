11ce1a48b5a11e8e314bd50d9e2d5a59e6a6dfa8
nazmul-hossin <mdnazmulhossin999@gmail.com>
2025-10-02 21:29:25 +0600

    Complete footer design

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,11 @@
+import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
 
 export default function Home() {
   return (
     <div className="bg-bgPrimary text-textPrimary">
       <Header />
-
+      <Footer />
     </div>
   );
 }
